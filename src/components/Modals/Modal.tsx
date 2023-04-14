99cea12e3dca8bfb0e49326597082936c74987bd
root <root@DESKTOP-6DBKV9K>
2023-04-14 13:34:40 +0000

    modal

--- src/components/Modals/Modal.tsx
+++ src/components/Modals/Modal.tsx
@@ -1,4 +1,6 @@
-import { FC, ReactElement } from "react";
+import { FC, ReactElement, useCallback, useEffect, useState } from "react";
+import { IoMdClose } from "react-icons/io";
+import Button from "../Button";
 
 interface ModalProps {
   isOpen?: boolean;
@@ -25,7 +27,89 @@ const Modal: FC<ModalProps> = ({
   secondaryAction,
   title,
 }) => {
-  return <div>Modal</div>;
+  const [showModal, setShowModal] = useState(isOpen);
+  useEffect(() => {
+    setShowModal(isOpen);
+  }, [isOpen]);
+
+  const handleClose = useCallback(() => {
+    if (disabled) {
+      return;
+    }
+
+    setShowModal(false);
+    setTimeout(() => {
+      onClose();
+    }, 300);
+  }, [onClose, disabled]);
+  const handleSubmit = useCallback(() => {
+    if (disabled) {
+      return;
+    }
+    onSubmit();
+  }, [onSubmit, disabled]);
+
+  const handleSecondaryAction = useCallback(() => {
+    if (disabled || !secondaryAction) {
+      return;
+    }
+    secondaryAction();
+  }, [secondaryAction, disabled]);
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <>
+      <div
+        className="overflow-x-hid fixed inset-0 z-50
+       flex items-center justify-center overflow-y-auto overflow-x-hidden bg-neutral-800/70 outline-none focus:outline-none"
+      >
+        <div className="relative mx-auto my-6 h-full w-full md:h-auto md:w-4/6 lg:h-auto lg:w-3/6 xl:w-2/5">
+          <div
+            className={`translate h-full duration-300 ${
+              showModal
+                ? "translate-y-0 opacity-100"
+                : "translate-y-full opacity-0"
+            }`}
+          >
+            <div className="translate relative flex h-full w-full flex-col rounded-lg border-0 bg-white shadow-lg outline-none focus:outline-none md:h-auto lg:h-auto">
+              {/* Header */}
+              <div className="relative flex items-center justify-center rounded-t border-b-[1px] p-6">
+                <button
+                  className="absolute left-9 border-0 p-1 transition hover:opacity-70"
+                  onClick={handleClose}
+                >
+                  <IoMdClose size={18} />
+                </button>
+                <div className="text-lg font-semibold">{title}</div>
+                <div className="relative flex-auto p-6">{body}</div>
+                <div className="flex flex-col gap-2 p-6">
+                  <div className="flex w-full flex-row items-center gap-4">
+                    {secondaryAction && secondaryActionlabel && (
+                      <Button
+                        disabled={disabled}
+                        label={secondaryActionlabel}
+                        onClick={handleSecondaryAction}
+                        outline
+                      />
+                    )}
+                    <Button
+                      disabled={disabled}
+                      label={actionLabel}
+                      onClick={handleSubmit}
+                    />
+                  </div>
+                  {footer}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Modal;
